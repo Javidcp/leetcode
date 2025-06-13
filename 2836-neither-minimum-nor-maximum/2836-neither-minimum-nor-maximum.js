180758a31e7a848a62eaab83143072382ccae9cf
Javidcp <143593890+Javidcp@users.noreply.github.com>
2025-06-13 14:12:40 +0530

    Time: 4 ms (94.27%), Space: 66 MB (64.06%) - LeetHub

---
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function(nums) {
+    if (nums.length <= 2) return -1;
+
+    let min = Math.min(...nums)
+    let max = Math.max(...nums)
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== min && nums[i] !== max) {
+            return nums[i]
+        }
+    }
+};
