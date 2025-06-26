b21ae6fad17ce0daf820c4cdd353a818d300c960
Javidcp <143593890+Javidcp@users.noreply.github.com>
2025-06-26 12:04:32 +0530

    Time: 1 ms (51.89%), Space: 57.7 MB (13.97%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+   if (num < 10) return num;
+   const numString = num.toString();
+   const numArray = numString.split('');
+   let sum = 0;
+   
+   for (const numStr of numArray) {
+       sum += parseInt(numStr);
+   }
+   
+   return addDigits(sum);
+};
