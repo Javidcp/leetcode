91a57aeaf9d5789ed23a70f15c358cdf9cb59cf0
Javidcp <143593890+Javidcp@users.noreply.github.com>
2025-07-03 09:33:30 +0530

    Time: 43 ms (49.76%), Space: 53.8 MB (22.49%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    return n-1
+    let ans = 0
+     while (n > 1) {
+            if (n % 2 == 0) {
+                ans += n / 2;
+                n = n / 2;
+            } else {
+                ans += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return ans;
 };
